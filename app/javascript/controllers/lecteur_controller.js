ffeac98855363be4b33592f4dd0fae01931994b6
Justin ! <just1602@riseup.net>
2022-08-09 10:49:52 -0400

    Make stimulus stuff works

---
+++ app/javascript/controllers/lecteur_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  connect() {
+    fetch('https://ecoutez.ckiafm.org:8001/json.xsl')
+      .then(response => {
+        if(!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        return response.json();
+      })
+      .then(data => {
+        const current_song = data.mounts["/live.mp3"].current_song;
+        if(current_song === ' -- non disponible') {
+          return;
+        }
+
+        const infos = current_song.split(' -- ');
+        console.log(infos[0].length)
+        if(infos[0].length > 0) {
+          console.log(`Vous écoutez ${infos[1]} de ${infos[0]}`)
+        } else {
+          console.log(`Vous écoutez ${infos[1]}`)
+        }
+      });
+  }
+};
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+pin 'application'
+pin 'admin'
+
 pin 'popper', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# If we want to split controllers from admin controllers, simply extract
+# `admin_controllers` in their own pin and load only the users visible
+# controllers under controllers
+pin_all_from "app/javascript/controllers", under: "controllers"
