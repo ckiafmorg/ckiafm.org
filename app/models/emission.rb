3e246b4f2da45f30db4fabf82d600271b1ca3ff3
ibrahim <2063828@cegeplimoilou.ca>
2023-02-17 14:39:33 -0500

    Ajout de la generation de slug à l'aide du gem friendly_id

--- app/models/emission.rb
+++ app/models/emission.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Emission < ApplicationRecord
+  extend FriendlyId
+  friendly_id :nom, use: :slugged
+
   STATUS = { brouillon: 0, active: 1, vacance: 2, archive: 3 }.freeze
   enum status: STATUS
 
