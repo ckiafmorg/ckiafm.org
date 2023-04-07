a72e5685153191246d0384dd6e1ba745e1ab17fd
Justin ! <just1602@riseup.net>
2023-04-07 13:33:58 -0400

    Keep the right schema in main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_201652) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_19_183439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -98,21 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_201652) do
     t.index ["nom"], name: "index_publicites_on_nom", unique: true
   end
 
-  create_table "taggables", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "emission_id", null: false
-    t.uuid "tags_emission_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["emission_id"], name: "index_taggables_on_emission_id"
-    t.index ["tags_emission_id"], name: "index_taggables_on_tags_emission_id"
-  end
-
-  create_table "tags_emissions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "nom"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest", null: false
@@ -126,6 +111,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_201652) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "diffusions", "emissions"
   add_foreign_key "emissions", "categorie_emissions"
-  add_foreign_key "taggables", "emissions"
-  add_foreign_key "taggables", "tags_emissions"
 end
