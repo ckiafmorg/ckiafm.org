81b2116bcbcfdfa42a664d11ab6c6cf5ae9d3fcd
Justin ! <just1602@riseup.net>
2024-02-10 16:04:33 -0500

    Display social media links in the public emission page

--- app/models/social_media_account.rb
+++ app/models/social_media_account.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SocialMediaAccount < ApplicationRecord
-  PLATFORMS = %i[facebook instagram twitter tiktok bluesky mastodon].freeze
+  PLATFORMS = %i[facebook instagram twitter tiktok mastodon].freeze
   enum :platform, PLATFORMS
 
   belongs_to :has_social_media_account, polymorphic: true
