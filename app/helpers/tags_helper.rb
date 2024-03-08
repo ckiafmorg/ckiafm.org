cef08a784ceb9e624676adce5d97fd106ed749a3
Justin ! <just1602@riseup.net>
2024-03-08 00:23:02 -0500

    Use table everywhere in the admin section

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module TagsHelper
+  def format_tags_list(tags)
+    return '' if tags.empty?
+
+    str = ''
+    tags.each do |tag|
+      if tag == tags.last
+        str += tag.nom
+      else
+        str += "#{tag.nom}, "
+      end
+    end
+    str
+  end
 end
