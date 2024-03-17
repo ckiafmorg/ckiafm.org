a352238b768c2e5bb03d1b2fde493e6029b0f29f
Justin ! <just1602@riseup.net>
2024-03-17 14:15:37 -0400

    Do some styling for the home page and public layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def show; end
+  def show
+    @current_emission = Diffusion.find_diffusion_en_cours&.first&.emission
+  end
 end
