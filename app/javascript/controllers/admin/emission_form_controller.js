b8951f2b9f5648277549b06f98ebee86cfb6904f
Justin ! <just1602@riseup.net>
2024-03-21 16:09:55 -0400

    Refactor emission nested_forms to have better js

--- app/javascript/controllers/admin/emission_form_controller.js
+++ app/javascript/controllers/admin/emission_form_controller.js
@@ -1,57 +1,49 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['socialNetworkFieldset', 'membreEquipeFieldset'];
+  static targets = ['equipeTemplate', 'equipeTarget', 'mediaSociauxTemplate', 'mediaSociauxTarget'];
 
-  createNewAccountForm(event) {
-    event.preventDefault();
+  addMediaSociaux(e) {
+    e.preventDefault();
 
-    const newFieldSet = this.socialNetworkFieldsetTarget.cloneNode(true);
-    const newId = this.socialNetworkFieldsetTargets.length;
-
-    // set newFieldSet platform field
-    newFieldSet.children[0].children[0].children[0].htmlFor = `emission_social_media_accounts_attributes_${newId}_platform`;
-    newFieldSet.children[0].children[0].children[1].id = `emission_social_media_accounts_attributes_${newId}_platform`;
-    newFieldSet.children[0].children[0].children[1].name = `emission[social_media_accounts_attributes][${newId}][platform]`;
-    newFieldSet.children[0].children[0].children[1].value = "facebook";
-
-    // set newFieldSet name field
-    newFieldSet.children[0].children[1].children[0].htmlFor = `emission_social_media_accounts_attributes_${newId}_nom`;
-    newFieldSet.children[0].children[1].children[1].id = `emission_social_media_accounts_attributes_${newId}_nom`;
-    newFieldSet.children[0].children[1].children[1].name = `emission[social_media_accounts_attributes][${newId}][nom]`;
-    newFieldSet.children[0].children[1].children[1].value = "";
-
-    // set newFieldSet url field
-    // TODO: find a cleaner way of accessing fields with stimulus?
-    newFieldSet.children[1].children[0].children[0].htmlFor = `emission_social_media_accounts_attributes_${newId}_url`;
-    newFieldSet.children[1].children[0].children[1].id = `emission_social_media_accounts_attributes_${newId}_url`;
-    newFieldSet.children[1].children[0].children[1].name = `emission[social_media_accounts_attributes][${newId}][url]`;
-    newFieldSet.children[1].children[0].children[1].value = "";
-
-
-    // // insert the newFieldSet
-    this.socialNetworkFieldsetTarget.parentNode.insertBefore(newFieldSet, this.socialNetworkFieldsetTargets[this.socialNetworkFieldsetTargets.length - 1]);
+    const content = this.mediaSociauxTemplateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString())
+    this.mediaSociauxTargetTarget.insertAdjacentHTML('beforebegin', content)
   }
 
-  createNewMembreForm(event) {
-    event.preventDefault();
+  deleteMediaSociaux(e) {
+    e.preventDefault();
 
-    const newFieldSet = this.membreEquipeFieldsetTarget.cloneNode(true);
-    const newId = this.membreEquipeFieldsetTargets.length;
+    const wrapper = e.target.closest(".media-sociaux-form-wrapper");
 
-    // set newFieldSet nom field
-    newFieldSet.children[0].children[0].children[0].htmlFor = `emission_membre_equipes_attributes_${newId}_nom`;
-    newFieldSet.children[0].children[0].children[1].id = `emission_membre_equipes_attributes_0_nom`;
-    newFieldSet.children[0].children[0].children[1].name = `emission[membre_equipes_attributes][${newId}][nom]`;
-    newFieldSet.children[0].children[0].children[1].value = '';
+    if (wrapper.dataset.newRecord === 'true') {
+      wrapper.remove();
+    } else {
+      wrapper.style.display = 'none';
 
-    // set newFieldSet role field
-    newFieldSet.children[0].children[1].children[0].htmlFor = `emission_membre_equipes_attributes_${newId}_role`;
-    newFieldSet.children[0].children[1].children[1].id = `emission_membre_equipes_attributes_${newId}}_role`;
-    newFieldSet.children[0].children[1].children[1].name = `emission[membre_equipes_attributes][${newId}][role]`;
-    newFieldSet.children[0].children[1].children[1].value = '';
+      const input = wrapper.querySelector("input[name*='_destroy']");
+      input.value = '1';
+    }
+  }
 
-    // // insert the newFieldSet
-    this.membreEquipeFieldsetTarget.parentNode.insertBefore(newFieldSet, this.membreEquipeFieldsetTargets[this.membreEquipeFieldsetTargets.length - 1]);
+  newMembre(e) {
+    e.preventDefault();
+
+    const content = this.equipeTemplateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString())
+    this.equipeTargetTarget.insertAdjacentHTML('beforebegin', content)
+  }
+
+  deleteMembre(e) {
+    e.preventDefault();
+
+    const wrapper = e.target.closest(".membre-equipe-form-wrapper");
+
+    if (wrapper.dataset.newRecord === 'true') {
+      wrapper.remove();
+    } else {
+      wrapper.style.display = 'none';
+
+      const input = wrapper.querySelector("input[name*='_destroy']");
+      input.value = '1';
+    }
   }
 }
