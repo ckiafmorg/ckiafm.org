1e1894448fd86e223b61b3e61df0c65b48e4cf25
Justin ! <just1602@riseup.net>
2024-10-04 11:24:18 -0400

    home: add notion of latest balado episode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@
 class HomeController < ApplicationController
   def show
     @current_emission = Diffusion.find_diffusion_en_cours&.first&.emission
+    @latest_balado_episode = BaladoEpisode.published.live.last&.includes(:podcast)
   end
 end
