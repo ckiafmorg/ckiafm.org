c82d9ee59ddf2cb1a806e7fe143eb239236a9670
Justin ! <just1602@riseup.net>
2024-10-10 23:08:08 -0400

    balado: add the possibility to browse one balado

--- app/controllers/balados_controller.rb
+++ app/controllers/balados_controller.rb
@@ -4,4 +4,8 @@ class BaladosController < ApplicationController
   def index
     @balados = Balado.published.live.includes(:episodes)
   end
+
+  def show
+    @balado = Balado.includes(:episodes).find(params[:id])
+  end
 end
