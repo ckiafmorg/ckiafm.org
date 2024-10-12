bb2956093ddd27b09f2609704eb78ff540b9b96f
Justin ! <just1602@riseup.net>
2024-10-11 23:53:12 -0400

    balados: allow to browse a balado and the list of episodes

--- app/controllers/balados_controller.rb
+++ app/controllers/balados_controller.rb
@@ -7,5 +7,7 @@ class BaladosController < ApplicationController
 
   def show
     @balado = Balado.includes(:episodes).find(params[:id])
+    @latest_episode = @balado.episodes.last
+    @other_episodes = @balado.episodes[1..] || []
   end
 end
